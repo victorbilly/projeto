f76e3f76b9cdd3908e3fc27b2a356183487549f9
Fernando Rech <nandorech@outlook.com>
2017-08-26 10:03:15 -0300

    Página contato concluída. Envio de e-mails corrigido!

--- envia.php
+++ envia.php
@@ -1,26 +1,33 @@
-﻿<?php
+<?php
 
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$mensagem = $_POST['mensagem'];
+// Passando os dados obtidos pelo formulário para as variáveis abaixo
+$nomeremetente     = $_POST['nome'];
+$emailremetente    = trim($_POST['email']);
+$emaildestinatario = 'contato@gamercollection.com.br';
+$assunto          = $_POST['assunto'];
+$mensagem          = $_POST['mensagem'];
+
  
-//No nosso caso é o e-mail do seu site ou que pertença a seu dominio
-$para = 'nandorech@outlook.com';
-$assunto = 'Contato do site';
-$cabecalho = "MIME-Version: 1.0" . "\r\n".
-             "Content-type: text/html; charset=iso-8859-1" . "\r\n".
-             "From: nandorech@outlook.com" . "\r\n" .
-             "Reply-To: nandorech@outlook.com";
+/* Montando a mensagem a ser enviada no corpo do e-mail. */
+$mensagemHTML = '
+<p><b>Nome:</b> '.$nomeremetente.'
+<p><b>E-Mail:</b> '.$emailremetente.'
+<p><b>Assunto:</b> '.$assunto.'
+<p><b>Mensagem:</b> '.$mensagem.'</p>
+<hr>';
+
+
+// O remetente deve ser um e-mail do seu domínio conforme determina a RFC 822.
+// O return-path deve ser ser o mesmo e-mail do remetente.
+$headers = "MIME-Version: 1.1\r\n";
+$headers .= "Content-type: text/html; charset=utf-8\r\n";
+$headers .= "From: $emailremetente\r\n"; // remetente
+$headers .= "Return-Path: $emailremetente \r\n"; // return-path
+$envio = mail($emaildestinatario, $assunto, $mensagemHTML, $headers); 
  
-$corpoDoEmail = $nome . "<br />";
-$corpoDoEmail .= $email . "<br /><br />";
-$corpoDoEmail .= $mensagem;
- 
-$status = mail($para, $assunto, $corpoDoEmail, $cabecalho);
- 
-if($status)
+if($envio){
     echo "<script> window.location.href = 'envio_concluido.html'; </script>";
-else
+}else{
     echo "<script> window.location.href = 'envio_erro.html'; </script>";
-
+ }
 ?>
